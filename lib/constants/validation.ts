081e3606e4d868504e9fa0d449ca7733120aaa40
Toni <tonirachev92@gmail.com>
2025-06-26 00:07:52 +0300

    feat: add validation rules for signup first and last name fields

--- lib/constants/validation.ts
+++ lib/constants/validation.ts
@@ -2,5 +2,15 @@ export const VALIDATION = {
     password: {
         minLength: 6,
         maxLength: 72,
+    },
+    signup: {
+        firstName: {
+            minLength: 2,
+            maxLength: 30,
+        },
+        lastName: {
+            minLength: 2,
+            maxLength: 30
+        },
     }
 }
