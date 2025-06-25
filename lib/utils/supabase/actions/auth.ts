b95d2708e5f681be27d2e0efa2e0c3c3cd988d29
Toni <tonirachev92@gmail.com>
2025-06-26 00:08:06 +0300

    feat: implement signup functionality with input validation and error handling in auth module

--- lib/utils/supabase/actions/auth.ts
+++ lib/utils/supabase/actions/auth.ts
@@ -3,11 +3,11 @@
 import { redirect } from "next/navigation";
 import { createClient } from "../server";
 import { revalidatePath } from "next/cache";
-import { LoginSchemaErrorType, LoginSchemaType } from "../validations/authSchema";
+import { LoginSchemaErrorType, LoginSchemaType, SignupSchemaErrorType, SignupSchemaType } from "../validations/authSchema";
 import { isAuthApiError } from "@supabase/supabase-js";
 import { createFormResult } from "../validations/utils";
 import { MESSAGES } from "@/lib/constants/messages";
-import { parseAndValidateInput, postLoginSuccess, signInUser } from "./helpers";
+import { parseAndValidateSigninInput, parseAndValidateSignupInput, postLoginSuccess, signInUser, signupUser } from "./helpers";
 
 type LoginStateType = {
     data: LoginSchemaType,
@@ -15,7 +15,7 @@ type LoginStateType = {
 }
 
 export async function login(state: LoginStateType, formData: FormData) {
-    const { data, result } = parseAndValidateInput(formData);
+    const { data, result } = parseAndValidateSigninInput(formData);
 
     if (!result.success) {
         return createFormResult(data as LoginSchemaType, result.error.formErrors as LoginSchemaErrorType)
@@ -29,6 +29,31 @@ export async function login(state: LoginStateType, formData: FormData) {
         }
 
         return createFormResult(data as LoginSchemaType, MESSAGES.genericError)
+    }
+
+    return postLoginSuccess();
+}
+
+type SignupActionType = {
+    data: SignupSchemaType,
+    errors: SignupSchemaErrorType
+}
+
+export async function signup(state: SignupActionType, formData: FormData) {
+    const { result, data } = parseAndValidateSignupInput(formData);
+
+    if (!result.success) {
+        return createFormResult(data as SignupSchemaType, result.error.formErrors as SignupSchemaErrorType)
+    }
+
+    try {
+        const userData = await signupUser(result.data);
+    } catch (error) {
+        if (isAuthApiError(error)) {
+            return createFormResult(result.data as SignupSchemaType, error.message)
+        }
+
+        return createFormResult(result.data, MESSAGES.genericError)
     }
 
     return postLoginSuccess();
