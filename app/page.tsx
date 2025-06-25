b09f75404c8352f94dba49ddf4003acf5e59730d
Toni <tonirachev92@gmail.com>
2025-06-25 12:43:45 +0300

    Implement signout functionality in Home component with a new button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { createClient } from "@/lib/utils/supabase/server"
+import { login, signout } from "@/lib/utils/supabase/actions/auth";
 
 export default async function Home() {
   return (
-    <div>
+    <div className="flex justify-between p-4">
       <h1>Home Page</h1>
+
+      <form>
+        <button formAction={signout}>Signout</button>
+      </form>
     </div>
   )
 }
