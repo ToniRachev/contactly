9508cc5b7b229dad5c8941842c63595ac208b481
Toni <tonirachev92@gmail.com>
2025-06-25 12:44:36 +0300

    Add utility functions for form error handling and result creation using Zod

---
+++ lib/utils/supabase/validations/utils.ts
@@ -0,0 +1,21 @@
+import { SafeParseError, ZodError, } from "zod"
+
+export const createFormError = (message: string) => {
+    const zodError = new ZodError([
+        {
+            code: 'custom',
+            message,
+            path: [],
+        }
+    ])
+    return zodError.formErrors
+}
+
+
+export const createFormResult = <T extends Record<string, any>>(
+    data: T,
+    errorData: ZodError<T>['formErrors'] | string
+) => ({
+    data,
+    errors: typeof errorData === 'string' ? createFormError(errorData) : errorData
+})
