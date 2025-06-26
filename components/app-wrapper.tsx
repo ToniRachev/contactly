8b58a4c89c9b7bb8b49be13b9d62863ccdab5da7
Toni <tonirachev92@gmail.com>
2025-06-26 23:39:06 +0300

    feat: integrate AppSidebar into AppWrapper with SidebarProvider for layout management

--- components/app-wrapper.tsx
+++ components/app-wrapper.tsx
@@ -2,6 +2,8 @@ import UserProvider from "@/lib/context/user";
 import { fetchUserProfile } from "@/lib/utils/supabase/actions/user/user";
 import { createClient } from "@/lib/utils/supabase/server";
 import { ReactNode } from "react";
+import AppSidebar from "./app-sidebar";
+import { SidebarProvider } from "./ui/sidebar";
 
 const getUserWithProfile = async () => {
     const supabase = await createClient();
@@ -14,7 +16,16 @@ export default async function AppWrapper({ children }: { children: ReactNode }) 
 
     return (
         <UserProvider userData={userData}>
-            {children}
+            <SidebarProvider>
+                <main className="w-full flex">
+                    <div className="">
+                        <AppSidebar />
+                    </div>
+                    <div className="w-full">
+                        {children}
+                    </div>
+                </main>
+            </SidebarProvider>
         </UserProvider>
     )
 }
