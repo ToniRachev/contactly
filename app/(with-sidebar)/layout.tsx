73842ebe1ab98313f3203c5b5f478ec3e87b0a2a
Toni <tonirachev92@gmail.com>
2025-06-27 20:42:09 +0300

    feat: add layout components for sidebar variations with compact and standard options

---
+++ app/(with-sidebar)/layout.tsx
@@ -0,0 +1,23 @@
+import AppSidebar from "@/components/app-sidebar";
+import ContentWrapper from "@/components/content-wrapper";
+import FriendsSidebar from "@/components/friends-sidebar";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+    return (
+        <div className="flex">
+            <div className="">
+                <AppSidebar />
+            </div>
+            <div className="flex-1 w-full">
+                <ContentWrapper>
+                    {children}
+                </ContentWrapper>
+            </div>
+
+            <div>
+                <FriendsSidebar />
+            </div>
+        </div>
+    )
+}
