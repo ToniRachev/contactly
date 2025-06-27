aaa0a4ac2f32cfd34274c97e3d56c7568e6a148a
Toni <tonirachev92@gmail.com>
2025-06-27 20:42:15 +0300

    feat: create Home page with post feed displaying user posts and interactions

---
+++ app/(with-sidebar)/page.tsx
@@ -0,0 +1,36 @@
+import Post from "@/components/post"
+
+const posts = [
+  {
+    avatar: '/user_avatar.webp',
+    authorName: 'Elias Monroe',
+    postedAt: '2 hours ago',
+    content: 'Just finished this piece after a week of late nights and too much tea ☕ It’s inspired by the feeling of getting lost in thought during a slow walk home. What do you see in it? I love hearing how others interpret my work.',
+    likes: 2598,
+    comments: 150,
+    isFollowingAuthor: true,
+  },
+  {
+    avatar: '/user_avatar.webp',
+    authorName: 'Elias Monroe',
+    postedAt: '2 days ago',
+    content: 'Just finished this piece after a week of late nights and too much tea ☕ It’s inspired by the feeling of getting lost in thought during a slow walk home. What do you see in it? I love hearing how others interpret my work.',
+    likes: 2598,
+    comments: 150,
+    isFollowingAuthor: false,
+  }
+]
+
+export default async function Home() {
+  return (
+    <div className="">
+      <h4>Feed</h4>
+
+      <div className="pt-12 flex flex-col gap-24 w-full items-center">
+        {posts.map((post) => (
+          <Post key={post.postedAt} {...post} />
+        ))}
+      </div>
+    </div>
+  )
+}
