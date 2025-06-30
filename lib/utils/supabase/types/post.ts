1c68d4b383a0620f19a34db572aadc22e1befd01
Toni <tonirachev92@gmail.com>
2025-06-30 13:52:58 +0300

    feat: add types for posts, comments, and authors in post.ts

---
+++ lib/utils/supabase/types/post.ts
@@ -0,0 +1,61 @@
+export type CountType = { count: number }[];
+export type LikesType = { user: string }[];
+
+export type AuthorDBType = {
+    id: string;
+    email: string;
+    first_name: string;
+    last_name: string;
+    created_at: string;
+}
+
+export type AuthorType = {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    createdAt: string;
+    fullName: string;
+}
+
+export type CommentDBType = {
+    id: string;
+    created_at: string;
+    author_id: string;
+    author: AuthorDBType;
+    post_id: string;
+    body: string;
+    likesCount: CountType;
+    likes: LikesType;
+}
+
+export type CommentType = {
+    id: string;
+    createdAt: string;
+    authorId: string;
+    author: AuthorType;
+    postId: string;
+    body: string;
+    likes: string[];
+    likesCount: number;
+}
+
+export type FeedDBType = {
+    id: string;
+    created_at: string;
+    author: AuthorDBType;
+    body: string;
+    commentsCount: CountType;
+    likesCount: CountType;
+    likes: LikesType;
+}
+
+export type FeedType = {
+    postId: string;
+    createdAt: string;
+    author: AuthorType;
+    body: string;
+    commentsCount: number;
+    likesCount: number;
+    likes: string[];
+}
