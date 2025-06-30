a0078aa83e4b670eb8ca137fd0df7fdeb8cdcf92
Toni <tonirachev92@gmail.com>
2025-06-30 21:39:01 +0300

    feat: create Posts component to display a list of posts using PostWrapper

---
+++ components/post-lists/posts.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { usePosts } from "@/lib/context/posts";
+import PostWrapper from "../post-wrapper";
+
+export default function Posts() {
+    const { posts } = usePosts();
+
+    return (
+        <div className="w-[50vw] grid gap-24">
+            {posts.map((post) => (
+                <PostWrapper
+                    key={post.postId}
+                    post={post}
+                />
+            ))}
+        </div>
+    )
+}
