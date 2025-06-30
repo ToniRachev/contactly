d516167ce5fea254fa779328b18e05c0a09e850b
Toni <tonirachev92@gmail.com>
2025-06-30 21:38:38 +0300

    feat: implement delete post functionality with loading state and action handling

--- components/delete-post.tsx
+++ components/delete-post.tsx
@@ -9,15 +9,33 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "./ui/button"
 import { DialogClose } from "@radix-ui/react-dialog";
-import { deletePost } from "@/lib/utils/supabase/actions/post/post";
+import { deletePostAction } from "@/lib/utils/supabase/actions/post/post";
+import { useActionState, useEffect, useState } from "react";
+import { usePosts } from "@/lib/context/posts";
 
 type DeletePostProps = {
     postId: string;
 }
 
 export default function DeletePost({ postId }: Readonly<DeletePostProps>) {
+    const [open, setOpen] = useState(false);
+
+    const { deletePost } = usePosts();
+
+    const actionWithPostId = deletePostAction.bind(null, postId)
+    const [state, formAction, isPending] = useActionState(actionWithPostId, {
+        success: false,
+    })
+
+    useEffect(() => {
+        if (state.success) {
+            deletePost(postId);
+            setOpen(false);
+        }
+    }, [state.success, deletePost, postId])
+
     return (
-        <Dialog>
+        <Dialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
                 <Button className="bg-stone-600 hover:bg-stone-500 min-w-[5vw]">Delete post</Button>
             </DialogTrigger>
@@ -31,10 +49,11 @@ export default function DeletePost({ postId }: Readonly<DeletePostProps>) {
                     <form className="w-full">
                         <Button
                             variant={'secondary'}
-                            formAction={deletePost.bind(null, postId)}
+                            formAction={formAction}
                             className="w-full"
+                            disabled={isPending}
                         >
-                            Delete
+                            {isPending ? 'Deleting...' : 'Delete'}
                         </Button>
                     </form>
                     <DialogClose asChild>
