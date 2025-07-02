acf586d93fbcaa89d108d8c6128ebec78ae60631
Toni <tonirachev92@gmail.com>
2025-07-02 15:29:47 +0300

    refactor: replace useState with useOptimistic in PostsProvider for improved state management

--- lib/context/posts.tsx
+++ lib/context/posts.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { createContext, ReactNode, useCallback, useContext, useMemo, useState } from "react";
+import { createContext, ReactNode, useCallback, useContext, useMemo, useOptimistic } from "react";
 import { PostType } from "../utils/supabase/types/post";
 
 type PostsContextType = {
@@ -8,7 +8,6 @@ type PostsContextType = {
     addPost: (post: PostType) => void;
     editPost: (postId: string, newContent: string) => void;
     deletePost: (postId: string) => void;
-    postReaction: (postId: string, userId: string, isLikedPost: boolean) => void;
 }
 
 const PostsContext = createContext<PostsContextType | null>(null);
@@ -19,7 +18,7 @@ type PostsProviderProps = {
 }
 
 export default function PostsProvider({ postsData, children }: Readonly<PostsProviderProps>) {
-    const [posts, setPosts] = useState<PostType[]>(postsData);
+    const [posts, setPosts] = useOptimistic<PostType[]>(postsData);
 
     const addPost = useCallback((post: PostType) => setPosts((prevPosts) => [post, ...prevPosts]), [setPosts]);
 
@@ -40,39 +39,12 @@ export default function PostsProvider({ postsData, children }: Readonly<PostsPro
         setPosts((prevPosts) => prevPosts.filter((post) => post.postId !== postId));
     }, [setPosts])
 
-    const postReaction = useCallback((postId: string, userId: string, isLikedPost: boolean) => {
-        setPosts((prevPosts) => {
-            const updatedPosts = [...prevPosts];
-            const postIndex = updatedPosts.findIndex((post) => post.postId === postId);
-
-            if (postIndex === -1) {
-                return prevPosts;
-            }
-
-            const post = updatedPosts[postIndex];
-
-            if (!isLikedPost) {
-                post.likesCount++
-                post.likes.push(userId);
-            } else {
-                post.likesCount--
-                post.likes = post.likes.filter((userLikedPostId) => userLikedPostId !== userId)
-            }
-
-            const updatedPost = { ...post };
-            updatedPosts[postIndex] = updatedPost;
-
-            return updatedPosts;
-        })
-    }, [setPosts])
-
     const contextValue = useMemo(() => ({
         posts,
         addPost,
         editPost,
         deletePost,
-        postReaction
-    }), [posts, addPost, editPost, deletePost, postReaction]);
+    }), [posts, addPost, editPost, deletePost]);
 
     return (
         <PostsContext.Provider value={contextValue}>
