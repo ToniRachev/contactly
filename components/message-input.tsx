7954f6af9df4058552516ed3143ebd70a84f8383
Toni <tonirachev92@gmail.com>
2025-07-03 15:18:21 +0300

    feat: extend MessageInput component to support error handling and additional textarea attributes

--- components/message-input.tsx
+++ components/message-input.tsx
@@ -1,14 +1,17 @@
 import { Textarea } from "@/components/ui/textarea";
 import clsx from "clsx";
+import { TextareaHTMLAttributes } from "react";
+import ErrorMessage from "./error-message";
 
 type MessageInputProps = {
     placeholder: string;
     name: string;
     value?: string;
     className?: string;
-}
+    error?: string;
+} & TextareaHTMLAttributes<HTMLTextAreaElement>
 
-export default function MessageInput({ placeholder, name, value = '', className }: Readonly<MessageInputProps>) {
+export default function MessageInput({ placeholder, name, value = '', className, error, ...props }: Readonly<MessageInputProps>) {
     return (
         <div className="relative w-full">
             <Textarea
@@ -19,7 +22,11 @@ export default function MessageInput({ placeholder, name, value = '', className 
              [&::-webkit-scrollbar]:w-2 
             [&::-webkit-scrollbar-track]:bg-surface
             [&::-webkit-scrollbar-thumb]:bg-[#8C8C8C]
-            [&::-webkit-scrollbar-button]:hidden`, className)} />
+                [&::-webkit-scrollbar-button]:hidden`, className)}
+                {...props}
+            />
+
+            {error && <ErrorMessage className='pt-2'>{error}</ErrorMessage>}
         </div>
     )
 }
