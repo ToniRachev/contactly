f2238beebc4791ccf915533e44d1f0ba66462704
Toni <tonirachev92@gmail.com>
2025-07-03 15:18:27 +0300

    feat: implement comment creation functionality in PostDetailedView component

--- components/post/post-detailed-view.tsx
+++ components/post/post-detailed-view.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
     Dialog,
     DialogContent,
@@ -7,14 +9,16 @@ import {
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden"
 import Post from "./post"
 import UserAvatar from "../user-avatar";
-import { ThumbsUp } from "lucide-react";
+import { Ellipsis, ThumbsUp } from "lucide-react";
 import { Separator } from "../ui/separator";
 import Filter from "../filter";
 import MessageInput from "../message-input";
-import { useEffect, useState } from "react";
-import { fetchPostComments } from "@/lib/utils/supabase/client/post";
+import { useActionState, useOptimistic, useRef } from "react";
 import { formatRelativeTime } from "@/lib/utils";
 import { CommentType, PostType } from "@/lib/utils/supabase/types/post";
+import { createCommentAction } from "@/lib/utils/supabase/actions/post/post.actions";
+import { CommentSchemaErrorType, CommentSchemaType } from "@/lib/utils/supabase/validations/postSchema";
+import { useUser } from "@/lib/context/user";
 
 const filters = [
     {
@@ -39,9 +43,26 @@ type PostViewProps = {
     controls: Controls;
     reaction: () => void;
     isLikedPost: boolean;
+    commentsData: CommentType[];
+}
+
+type CreateCommentProps = {
+    postId: string;
+}
+
+const CommentControls = () => {
+    return (
+        <div className="">
+            <Ellipsis />
+        </div>
+    )
 }
 
 const Comment = ({ comment }: Readonly<{ comment: CommentType }>) => {
+    const { user } = useUser();
+
+    const isCommentAuthor = user?.id === comment.author.id;
+
     return (
         <div className="flex flex-col gap-2">
             <div className="flex gap-2 w-full">
@@ -51,9 +72,15 @@ const Comment = ({ comment }: Readonly<{ comment: CommentType }>) => {
                 />
 
                 <div className="w-full">
-                    <div className="bg-surface w-full p-4 rounded-lg">
-                        <p className="text-sm pb-4">{comment.author.fullName}</p>
-                        <p>{comment.body}</p>
+                    <div className="bg-surface w-full p-4 rounded-lg flex justify-between">
+                        <div>
+                            <p className="text-sm pb-4">{comment.author.fullName}</p>
+                            <p>{comment.body}</p>
+                        </div>
+
+                        {isCommentAuthor && (
+                            <CommentControls />
+                        )}
                     </div>
 
                     <div className="flex items-center justify-between p-2">
@@ -72,19 +99,50 @@ const Comment = ({ comment }: Readonly<{ comment: CommentType }>) => {
     )
 }
 
-export function PostDetailedView({ post, controls, reaction, isLikedPost }: Readonly<PostViewProps>) {
-    const [comments, setComments] = useState<CommentType[]>([]);
 
-    useEffect(() => {
-        if (controls.open) {
-            (async function () {
-                const comments = await fetchPostComments(post.postId);
-                setComments(comments);
-            })()
+const CreateComment = ({ postId }: Readonly<CreateCommentProps>) => {
+    const { user } = useUser();
+    const formRef = useRef<HTMLFormElement>(null);
+
+    const createCommentActionWithUserAndPostId = createCommentAction.bind(null, postId, user?.id ?? '');
+
+    const [state, formAction, isPending] = useActionState(createCommentActionWithUserAndPostId, {
+        data: {
+            body: '',
+        } as CommentSchemaType,
+        errors: {} as CommentSchemaErrorType,
+        newComment: null,
+        success: false
+    })
+
+    const handleSubmitComment = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            if (formRef.current) {
+                formRef.current.requestSubmit();
+            }
         }
+    }
 
-        return () => setComments([]);
-    }, [post.postId, controls.open])
+    return (
+        <div className="w-full flex gap-2 items-center sticky bottom-0 bg-background py-4">
+            <UserAvatar avatar={'/user_avatar.webp'} width={32} height={32} />
+            <form ref={formRef} action={formAction} className="w-full">
+                <MessageInput
+                    placeholder="Add a comment..."
+                    name="body"
+                    className=" w-full"
+                    onKeyDown={handleSubmitComment}
+                    error={state.errors.fieldErrors?.body?.[0]}
+                    disabled={isPending}
+                />
+            </form>
+        </div>
+    )
+}
+
+export function PostDetailedView({ post, controls, reaction, isLikedPost, commentsData }: Readonly<PostViewProps>) {
+    const [comments,] = useOptimistic<CommentType[]>(commentsData);
 
     return (
         <Dialog open={controls.open} onOpenChange={controls.setState}>
@@ -109,15 +167,7 @@ export function PostDetailedView({ post, controls, reaction, isLikedPost }: Read
                         <Comment key={comment.id} comment={comment} />
                     ))}
 
-                    <div className="w-full flex gap-2 items-center sticky bottom-0 bg-background py-4">
-                        <UserAvatar avatar={'/user_avatar.webp'} width={32} height={32} />
-
-                        <MessageInput
-                            placeholder="Add a comment..."
-                            name="comment"
-                            className="w-full"
-                        />
-                    </div>
+                    <CreateComment postId={post.postId} />
                 </div>
             </DialogContent>
         </Dialog>
