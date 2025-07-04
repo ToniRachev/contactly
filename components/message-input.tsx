d785184cdfb171212bedfa8c2c75f73cc36b6718
Toni <tonirachev92@gmail.com>
2025-07-04 11:48:15 +0300

    feat: extend MessageInput component to accept a ref prop for improved textarea handling

--- components/message-input.tsx
+++ components/message-input.tsx
@@ -9,9 +9,10 @@ type MessageInputProps = {
     value?: string;
     className?: string;
     error?: string;
+    ref?: React.RefObject<HTMLTextAreaElement> | null;
 } & TextareaHTMLAttributes<HTMLTextAreaElement>
 
-export default function MessageInput({ placeholder, name, value = '', className, error, ...props }: Readonly<MessageInputProps>) {
+export default function MessageInput({ placeholder, name, value = '', className, error, ref, ...props }: Readonly<MessageInputProps>) {
     return (
         <div className="relative w-full">
             <Textarea
@@ -23,6 +24,7 @@ export default function MessageInput({ placeholder, name, value = '', className,
             [&::-webkit-scrollbar-track]:bg-surface
             [&::-webkit-scrollbar-thumb]:bg-[#8C8C8C]
                 [&::-webkit-scrollbar-button]:hidden`, className)}
+                ref={ref}
                 {...props}
             />
 
