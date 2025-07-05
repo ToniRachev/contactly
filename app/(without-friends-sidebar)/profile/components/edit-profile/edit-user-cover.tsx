a9d8074760f2e30f2d973a30b53a5a534b04ee53
Toni <tonirachev92@gmail.com>
2025-07-05 15:07:23 +0300

    feat: update EditUserCover component to use EditUserImage for cover photo uploads and manage image state

--- app/(without-friends-sidebar)/profile/components/edit-profile/edit-user-cover.tsx
+++ app/(without-friends-sidebar)/profile/components/edit-profile/edit-user-cover.tsx
@@ -3,24 +3,42 @@
 import Image from "next/image";
 import { SectionWrapper } from ".";
 import { Camera } from "lucide-react";
-import ImageUploadWrapper from "@/components/image-upload-wrapper";
+import EditUserImage from "./edit-user-image";
+import { useUser } from "@/lib/context/user.context";
+import { useCallback, useState } from "react";
 
 export default function EditUserCover() {
+    const { user, updateUserCover } = useUser();
+    const [image, setImage] = useState<string | null>(user.coverUrl);
 
-    const handleImageChange = (file: File) => {
-        console.log(file);
+    const handleImageChange = (imageUrl: string) => {
+        setImage(imageUrl);
     }
+
+    const handleImageSuccess = useCallback((imageUrl: string) => {
+        updateUserCover(imageUrl);
+    }, [updateUserCover])
 
     return (
         <SectionWrapper title="Edit cover photo" className="justify-center">
-            <div className="relative w-full flex h-[25svh]">
-                <ImageUploadWrapper name="cover" onImageChange={handleImageChange}>
-                    <Image src={'/user_profile_background.png'} alt="User cover photo" className="object-cover" fill />
+            <EditUserImage
+                imageType="cover"
+                onImageChange={handleImageChange}
+                onSuccess={handleImageSuccess}
+            >
+                <div className="relative w-full max-w-[100vw] flex h-[25svh]">
+
+                    {image ? (
+                        <Image src={image} alt="User cover photo" className="object-cover" fill />
+                    ) : (
+                        <div className="w-full h-full absolute inset-0 bg-surface" />
+                    )}
+
                     <div className="absolute bottom-0 right-0 bg-stone-600 p-1 rounded-full">
                         <Camera />
                     </div>
-                </ImageUploadWrapper>
-            </div>
+                </div>
+            </EditUserImage>
         </SectionWrapper>
     )
 }
