ae7fa9d75cfffc210a9d404d2a0086bce23c54cd
Toni <tonirachev92@gmail.com>
2025-07-05 15:07:18 +0300

    refactor: simplify EditProfilePicture component by removing form handling and integrating EditUserImage for avatar updates

--- app/(without-friends-sidebar)/profile/components/edit-profile/edit-profile-picture.tsx
+++ app/(without-friends-sidebar)/profile/components/edit-profile/edit-profile-picture.tsx
@@ -3,74 +3,42 @@
 import Avatar from "@/components/user-avatar";
 import { SectionWrapper } from ".";
 import { Camera } from "lucide-react";
-import ImageUploadWrapper from "@/components/image-upload-wrapper";
 import { useUser } from "@/lib/context/user.context";
-import { useActionState, useEffect, useRef, useState } from "react";
-import { updateUserAvatarAction } from "@/lib/actions/user/user.actions";
-import { UpdateUserAvatarSchemaErrorType, updateUserAvatarSchema } from "@/lib/validations/userSchema";
+import { useCallback, useState } from "react";
+import EditUserImage from "./edit-user-image";
 
 export default function EditProfilePicture() {
-    const { user, updateUserAvatar } = useUser();
-    const formRef = useRef<HTMLFormElement>(null);
-    const [validateError, setValidateError] = useState<string | null>(null);
-
-    const updateUserAvatarActionWithUserId = updateUserAvatarAction.bind(null, user.id);
-
-    const [state, formAction, isPending] = useActionState(updateUserAvatarActionWithUserId, {
-        success: false,
-        errors: {} as UpdateUserAvatarSchemaErrorType,
-        avatarUrl: null
-    });
-
+    const { user } = useUser();
     const [image, setImage] = useState<string | null>(user.avatarUrl);
-    const error = validateError ?? state.errors?.fieldErrors?.avatar?.[0] ?? state.errors?.formErrors?.[0];
 
-    const handleImageChange = (file: File) => {
-        const validateFile = updateUserAvatarSchema.safeParse({
-            avatar: file,
-        })
+    const { updateUserAvatar } = useUser();
 
-        if (!validateFile.success) {
-            setValidateError(validateFile.error?.formErrors?.fieldErrors?.avatar?.[0] ?? null);
-            return;
-        }
-
-        if (formRef.current) {
-            setValidateError(null);
-            setImage(URL.createObjectURL(file));
-            formRef.current.requestSubmit();
-        }
+    const handleImageChange = (imageUrl: string) => {
+        setImage(imageUrl);
     }
 
-    useEffect(() => {
-        if (state.success && state.avatarUrl) {
-            updateUserAvatar(state.avatarUrl);
-        }
-    }, [state.success, state.avatarUrl, updateUserAvatar])
-
-
+    const handleImageSuccess = useCallback((imageUrl: string) => {
+        updateUserAvatar(imageUrl);
+    }, [updateUserAvatar])
 
     return (
         <SectionWrapper title="Edit profile picture" className="justify-center">
             <div className="">
-                <form ref={formRef} action={formAction}>
-                    <ImageUploadWrapper
-                        name="avatar"
-                        onImageChange={handleImageChange}
-                        error={error}
-                        disabled={isPending}
-                    >
-                        <div className="relative">
-                            <Avatar
-                                avatar={image}
-                                size={'lg'}
-                            />
-                            <div className="absolute bottom-0 right-0 bg-stone-600 p-1 rounded-full">
-                                <Camera />
-                            </div>
+                <EditUserImage
+                    imageType="avatar"
+                    onImageChange={handleImageChange}
+                    onSuccess={handleImageSuccess}
+                >
+                    <div className="relative">
+                        <Avatar
+                            avatar={image}
+                            size={'lg'}
+                        />
+                        <div className="absolute bottom-0 right-0 bg-stone-600 p-1 rounded-full">
+                            <Camera />
                         </div>
-                    </ImageUploadWrapper>
-                </form>
+                    </div>
+                </EditUserImage>
             </div>
         </SectionWrapper>
     )
