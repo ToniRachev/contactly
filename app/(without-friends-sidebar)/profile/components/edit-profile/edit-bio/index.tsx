2a4719c80754339d87fd803a8fa76ddb1c66fc51
Toni <tonirachev92@gmail.com>
2025-07-08 11:53:04 +0300

    feat: add EditBio component for user biography editing with fields for hometown, current city, high school, and birth date

---
+++ app/(without-friends-sidebar)/profile/components/edit-profile/edit-bio/index.tsx
@@ -0,0 +1,105 @@
+'use client';
+
+import { useUser } from "@/lib/context/user.context";
+import { SectionWrapper } from "..";
+import {
+    UpdateHometownSchemaType,
+    UpdateHometownSchemaErrorType,
+    UpdateCurrentCitySchemaErrorType,
+    UpdateCurrentCitySchemaType,
+    UpdateHighSchoolSchemaErrorType,
+    UpdateHighSchoolSchemaType,
+    UpdateBirthDateSchemaErrorType,
+    UpdateBirthDateSchemaType
+} from "@/lib/validations/userSchema";
+import EditField from "./edit-field";
+import { useCallback, useMemo } from "react";
+import { Building2, Calendar, Home, School } from "lucide-react";
+import { FieldConfig } from "./types";
+
+export default function EditBio() {
+    const { user, updateUserBioField } = useUser();
+
+    const handleUpdateBioSuccess = useCallback((field: string, newValue: string) => {
+        updateUserBioField(field, newValue);
+    }, [updateUserBioField]);
+
+    const birthDate = user.biography.birthDate ? new Date(user.biography.birthDate) : undefined;
+
+    const editFields: FieldConfig[] = useMemo(() => [
+        {
+            name: 'hometown',
+            data: user.biography.hometown,
+            label: 'Hometown',
+            dbField: 'hometown',
+            icon: <Home />,
+            type: 'text',
+            placeholder: 'Add hometown',
+            initialState: {
+                data: {
+                    hometown: user.biography.hometown,
+                } as UpdateHometownSchemaType,
+                success: false,
+                errors: {} as UpdateHometownSchemaErrorType,
+            },
+        },
+        {
+            name: 'currentCity',
+            data: user.biography.currentCity,
+            label: 'Current city',
+            dbField: 'current_city',
+            icon: <Building2 />,
+            type: 'text',
+            placeholder: 'Add current city',
+            initialState: {
+                data: {
+                    currentCity: user.biography.currentCity,
+                } as UpdateCurrentCitySchemaType,
+                success: false,
+                errors: {} as UpdateCurrentCitySchemaErrorType,
+            },
+        },
+        {
+            name: 'school',
+            dbField: 'school',
+            data: user.biography.school,
+            label: 'High school',
+            icon: <School />,
+            type: 'text',
+            placeholder: 'Add high school',
+            initialState: {
+                data: {
+                    school: user.biography.school,
+                } as UpdateHighSchoolSchemaType,
+                success: false,
+                errors: {} as UpdateHighSchoolSchemaErrorType,
+            },
+        },
+        {
+            name: 'birthDate',
+            data: user.biography.birthDate,
+            icon: <Calendar />,
+            type: 'date',
+            placeholder: 'Add birth date',
+            dbField: 'birth_date',
+            label: 'Birth date',
+            initialState: {
+                data: {
+                    birthDate: birthDate,
+                } as UpdateBirthDateSchemaType,
+                success: false,
+                errors: {} as UpdateBirthDateSchemaErrorType,
+            },
+        }
+    ], [user]);
+
+    return (
+        <SectionWrapper title="Edit your bio">
+            <ul className="w-full">
+                {editFields.map((field) => (
+                    <EditField key={field.name} config={field} callback={handleUpdateBioSuccess} />
+                ))}
+            </ul>
+        </SectionWrapper>
+    )
+}
