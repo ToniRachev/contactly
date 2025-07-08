4545751d6d684af9fd5cce966d7f31a5c043cedd
Toni <tonirachev92@gmail.com>
2025-07-08 18:45:26 +0300

    feat: add DeleteButton component for removing user biography fields in edit profile

---
+++ app/(without-friends-sidebar)/profile/components/edit-profile/edit-bio/delete-button.tsx
@@ -0,0 +1,35 @@
+import { useActionState, useEffect } from "react";
+import { Trash2 } from "lucide-react";
+import { deleteUserBioFieldAction } from "@/lib/actions/user/user.actions";
+import { FieldConfig } from "./types";
+import { useUser } from "@/lib/context/user.context";
+
+type DeleteButtonProps = {
+    config: FieldConfig;
+    closeEditing: () => void;
+}
+
+export default function DeleteButton({ config, closeEditing }: Readonly<DeleteButtonProps>) {
+
+    const { updateUserBioField } = useUser();
+    const deleteActionWrapper = deleteUserBioFieldAction.bind(null, config.dbField);
+    const [deleteState, deleteFormAction, isDeletePending] = useActionState(deleteActionWrapper, {
+        error: null,
+        success: false
+    });
+
+    useEffect(() => {
+        if (deleteState.success) {
+            updateUserBioField(config.name, '');
+            closeEditing();
+        }
+    }, [deleteState, config.name, updateUserBioField, closeEditing])
+
+    return (
+        <form className="w-full h-full">
+            <button formAction={deleteFormAction} disabled={isDeletePending}>
+                <Trash2 width={20} className="cursor-pointer" />
+            </button>
+        </form>
+    )
+}
