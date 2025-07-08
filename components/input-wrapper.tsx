d59312bd549b79e5b23ad4cc6e89d680a5352849
Toni <tonirachev92@gmail.com>
2025-07-08 11:53:27 +0300

    feat: enhance FormInput component to accept custom className for improved styling flexibility

--- components/input-wrapper.tsx
+++ components/input-wrapper.tsx
@@ -2,17 +2,19 @@ import { InputHTMLAttributes } from "react";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import ErrorMessage from "./error-message";
+import clsx from "clsx";
 
 type FormInputProps = {
     label: string,
     error?: string,
+    className?: string,
 } & InputHTMLAttributes<HTMLInputElement>
 
-export default function FormInput({ label, error, ...props }: FormInputProps) {
+export default function FormInput({ label, error, className, ...props }: FormInputProps) {
     return (
         <div className="grid gap-3 items-center">
             <Label htmlFor={props.name}>{label}</Label>
-            <Input {...props} className="placeholder:text-stone-300"/>
+            <Input {...props} className={clsx("placeholder:text-stone-300", className)} />
             {error && (
                 <ErrorMessage>
                     {error}
