e900b0e040634d4444931dd549b933c9b9fe9b14
Toni <tonirachev92@gmail.com>
2025-07-09 11:18:35 +0300

    feat: integrate friend send requests into AppWrapper for enhanced user experience

--- components/app-wrapper.tsx
+++ components/app-wrapper.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/lib/utils/supabase/server";
 import { ReactNode } from "react";
 import { SidebarProvider } from "./ui/sidebar";
 import FriendsContextProvider from "@/lib/context/friends.context";
+import { getFriendsSendRequests } from "@/lib/actions/friendship/friendship.actions";
 
 const getUserWithProfile = async () => {
     const supabase = await createClient();
@@ -13,10 +14,11 @@ const getUserWithProfile = async () => {
 
 export default async function AppWrapper({ children }: Readonly<{ children: ReactNode }>) {
     const userData = await getUserWithProfile();
+    const friendsSendRequests = userData ? await getFriendsSendRequests(userData.id) : [];
 
     return (
         <UserProvider userData={userData}>
-            <FriendsContextProvider>
+            <FriendsContextProvider friendSendRequests={friendsSendRequests}>
                 <SidebarProvider>
                     <main className="w-full">
                         {children}
