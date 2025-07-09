5e4efedb3053d10578a1a27d322e6d2cda0b9948
Toni <tonirachev92@gmail.com>
2025-07-09 11:18:56 +0300

    feat: implement friendship actions for sending, canceling, and retrieving friend requests

---
+++ lib/actions/friendship/friendship.actions.ts
@@ -0,0 +1,69 @@
+'use server';
+
+import { baseFetcher } from "@/lib/utils/supabase/helpers";
+import { createClient } from "@/lib/utils/supabase/server"
+
+export async function sendFriendRequest(senderId: string, receiverId: string) {
+    const supabase = await createClient();
+
+    try {
+        await baseFetcher(
+            supabase.from('friend_requests')
+                .insert([{
+                    sender_id: senderId,
+                    receiver_id: receiverId
+                }])
+        )
+
+        return {
+            success: true,
+            message: 'Friend request sent successfully'
+        }
+    } catch (error) {
+        console.error(error);
+
+        return {
+            success: false,
+            message: 'Failed to send friend request'
+        }
+    }
+}
+
+export async function cancelFriendRequest(senderId: string, receiverId: string) {
+    const supabase = await createClient();
+
+    try {
+        await baseFetcher(
+            supabase.from('friend_requests')
+                .delete()
+                .match({
+                    sender_id: senderId,
+                    receiver_id: receiverId
+                })
+        )
+
+        return {
+            success: true,
+            message: 'Friend request cancelled successfully'
+        }
+    } catch (error) {
+        console.error(error);
+
+        return {
+            success: false,
+            message: 'Failed to cancel friend request'
+        }
+    }
+}
+
+export async function getFriendsSendRequests(userId: string) {
+    const supabase = await createClient();
+
+    const data = await baseFetcher(
+        supabase.from('friend_requests')
+            .select('receiver_id')
+            .eq('sender_id', userId)
+    )
+
+    return data.map((request) => request.receiver_id);
+}
