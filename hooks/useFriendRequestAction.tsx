0d2d3a8f246bc5b7430917422ac4197221b98049
Toni <tonirachev92@gmail.com>
2025-07-13 11:44:36 +0300

    refactor: extend useFriendRequestAction to include 'remove' action and update parameter names for clarity

--- hooks/useFriendRequestAction.tsx
+++ hooks/useFriendRequestAction.tsx
@@ -1,29 +1,31 @@
 'use client';
 
-import { acceptFriendRequest, deleteFriendRequest, sendFriendRequest } from "@/lib/actions/friendship/friendship.actions";
+import { acceptFriendRequest, deleteFriendRequest, removeFriend, sendFriendRequest } from "@/lib/actions/friendship/friendship.actions";
 import { useActionState, useEffect } from "react";
 
 type FriendRequestActionProps = {
-    type: 'accept' | 'decline' | 'send';
-    senderId: string;
-    receiverId: string;
+    type: 'accept' | 'decline' | 'send' | 'remove';
+    userId: string;
+    friendId: string;
     onSuccess?: () => void;
 }
 
 export default function useFriendRequestAction({
     type,
-    senderId,
-    receiverId,
+    userId,
+    friendId,
     onSuccess
 }: Readonly<FriendRequestActionProps>) {
     const getHandler = () => {
         switch (type) {
             case 'accept':
-                return acceptFriendRequest.bind(null, senderId, receiverId);
+                return acceptFriendRequest.bind(null, userId, friendId);
             case 'decline':
-                return deleteFriendRequest.bind(null, senderId, receiverId);
+                return deleteFriendRequest.bind(null, userId, friendId);
             case 'send':
-                return sendFriendRequest.bind(null, senderId, receiverId);
+                return sendFriendRequest.bind(null, userId, friendId);
+            case 'remove':
+                return removeFriend.bind(null, userId, friendId);
             default:
                 throw new Error('Invalid type');
         }
