bd8e269ee650c9580c9dde7728e4bdcd1c7295c3
Toni <tonirachev92@gmail.com>
2025-07-15 20:55:37 +0300

    refactor: change Layout component to use Readonly type for children prop

--- app/(without-sidebar)/(auth)/layout.tsx
+++ app/(without-sidebar)/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({ children }: Readonly<{ children: ReactNode }>) {
     return (
         <div className="min-h-screen bg-[url('/background_auth.webp')] bg-cover">
             {children}
