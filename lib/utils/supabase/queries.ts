0dd75706aebd1318c68bba18869fed068474fc99
Toni <tonirachev92@gmail.com>
2025-07-22 16:44:22 +0300

    feat: add user query constants

---
+++ lib/utils/supabase/queries.ts
@@ -0,0 +1,5 @@
+export const baseUserQuery = 'id, first_name, last_name, avatar_url, created_at, email';
+
+export const userQueryWithPresenceStatus = `${baseUserQuery}, presence_status, last_seen`;
+
+export const userQueryWithBiography = `${baseUserQuery}, biography(*)`;
