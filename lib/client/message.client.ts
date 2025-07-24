8e8f99fdfcb3f82aacd62cf45ec9528518d72026
Toni <tonirachev92@gmail.com>
2025-07-24 11:55:22 +0300

    feat: add functionality to read messages and update last read status in conversation participants

--- lib/client/message.client.ts
+++ lib/client/message.client.ts
@@ -7,14 +7,33 @@ export async function createOrFindConversation(userIds: string[]) {
 
     const [user1_id, user2_id] = userIds.toSorted((a, b) => a.localeCompare(b));
 
-    const conversation = await baseFetcher(
+    const conversationData = await baseFetcher(
         supabase.from('conversations').upsert({
             user1_id,
             user2_id
         }, {
             onConflict: 'user1_id,user2_id'
-        }).select('*, messages(*)')
+        }).select('id').single()
+    )
+
+    const conversation = await baseFetcher(
+        supabase.from('conversations')
+            .select('*, messages(*), conversation_participants(*)')
+            .eq('id', conversationData.id)
     )
 
     return transformConversation(conversation[0]);
+}
+
+export async function readMessage(conversationId: string, userId: string, lastMessageId: string) {
+    const supabase = createClient();
+
+    await baseFetcher(supabase.from('conversation_participants')
+        .update({
+            last_read_message_id: lastMessageId,
+            last_read_at: new Date(),
+        })
+        .eq('conversation_id', conversationId)
+        .eq('user_id', userId)
+    )
 }
