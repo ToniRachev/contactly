804d0d99055c5e9cce4e189c1d64c6514fdc03ae
Toni <tonirachev92@gmail.com>
2025-08-02 11:01:14 +0300

    feat: create UserHeader component for displaying user avatar and full name in post creation

---
+++ components/create-post/user-header.tsx
@@ -0,0 +1,19 @@
+import Avatar from "../user-avatar";
+
+type UserHeaderProps = {
+    avatarUrl: string | null;
+    fullName: string;
+}
+
+export default function UserHeader({ avatarUrl, fullName }: Readonly<UserHeaderProps>) {
+    return (
+        <div className="flex items-center gap-2">
+            <Avatar
+                avatar={avatarUrl}
+                size={'sm'}
+            />
+
+            <p>{fullName}</p>
+        </div>
+    )
+}
