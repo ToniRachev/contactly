46ffc5b557a59f61526232bb550891812d27a3ef
Toni <tonirachev92@gmail.com>
2025-08-09 19:34:12 +0300

    feat: add post query to enhance data retrieval with comments and likes counts, and include author and album details

--- lib/utils/supabase/queries.ts
+++ lib/utils/supabase/queries.ts
@@ -3,3 +3,17 @@ export const baseUserQuery = 'id, first_name, last_name, avatar_url, created_at,
 export const userQueryWithPresenceStatus = `${baseUserQuery}, presence_status, last_seen`;
 
 export const userQueryWithBiography = `${baseUserQuery}, biography(*), cover_url`;
+
+export const postQuery = `
+  *,
+  commentsCount:comments(count), 
+  likesCount:likes_posts(count),
+  likes:likes_posts(user:user_id),
+  author:author_id(
+    ${baseUserQuery}
+  ),
+  album:album_id(
+    *, 
+    photos(*)
+  )
+`;
