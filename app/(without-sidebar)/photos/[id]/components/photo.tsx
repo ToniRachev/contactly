f7597b00272ac7187a53e73970c80d973a2ea9f0
Toni <tonirachev92@gmail.com>
2025-08-13 20:23:54 +0300

    refactor: update Photo component to utilize PhotoType and integrate photoReaction handling for enhanced user interaction

--- app/(without-sidebar)/photos/[id]/components/photo.tsx
+++ app/(without-sidebar)/photos/[id]/components/photo.tsx
@@ -1,27 +1,44 @@
+'use client';
+
 import { BaseUserType } from "@/lib/types/user";
+import { PhotoType } from "@/lib/types/photos";
 import Image from "next/image";
 import PhotoMetadata from "./photo-metadata";
+import { useAuthenticatedUser } from "@/lib/context/user.context";
 
 type PhotoProps = {
-    url: string;
     author: BaseUserType;
-    createdAt: string;
+    photo: PhotoType;
+    photoReaction: {
+        handleOptimisticPhotoReaction: (photoId: string, isLikedPhoto: boolean, userId: string) => void;
+        updateLocalPhotoReaction: (photoId: string, isLikedPhoto: boolean, userId: string) => void;
+    }
 }
 
-export default function Photo({ url, author, createdAt }: Readonly<PhotoProps>) {
+export default function Photo({ photo, author, photoReaction }: Readonly<PhotoProps>) {
+    const { user } = useAuthenticatedUser();
+
+    const isLikedPhoto = photo.likes.includes(user.id);
     return (
-        <div className="flex gap-4">
+        <div className="">
             <Image
-                src={url}
+                src={photo.url}
                 alt="Add Alt later"
-                width={500}
+                width={750}
                 height={500}
-                className="h-[100svh] w-[40svw] object-cover"
+                className="h-[100svh] object-cover"
             />
 
             <PhotoMetadata
                 author={author}
-                createdAt={createdAt}
+                photoData={{
+                    createdAt: photo.createdAt,
+                    caption: photo.caption,
+                    photoId: photo.id,
+                    isLikedPhoto,
+                    likesCount: photo.likes.length,
+                }}
+                photoReaction={photoReaction}
             />
         </div>
     )
