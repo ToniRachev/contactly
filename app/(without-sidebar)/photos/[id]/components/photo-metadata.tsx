0e1060eda3d45069054af36f36760822cc409db6
Toni <tonirachev92@gmail.com>
2025-08-13 20:23:43 +0300

    refactor: update PhotoMetadata component to accept photoData and photoReaction props for enhanced functionality and display of photo details

--- app/(without-sidebar)/photos/[id]/components/photo-metadata.tsx
+++ app/(without-sidebar)/photos/[id]/components/photo-metadata.tsx
@@ -1,22 +1,46 @@
+'use client';
+
 import Avatar from "@/components/user-avatar";
 import { BaseUserType } from "@/lib/types/user";
 import { formatFullName, formatRelativeTime } from "@/lib/utils";
+import PhotoReaction from "./photo-reaction";
 
 type PhotoMetadataProps = {
     author: BaseUserType;
-    createdAt: string;
+    photoData: {
+        createdAt: string;
+        caption: string | null;
+        photoId: string
+        isLikedPhoto: boolean;
+        likesCount: number;
+    }
+    photoReaction: {
+        handleOptimisticPhotoReaction: (photoId: string, isLikedPhoto: boolean, userId: string) => void;
+        updateLocalPhotoReaction: (photoId: string, isLikedPhoto: boolean, userId: string) => void;
+    }
 }
 
-export default function PhotoMetadata({ author, createdAt }: Readonly<PhotoMetadataProps>) {
+export default function PhotoMetadata({ author, photoData, photoReaction }: Readonly<PhotoMetadataProps>) {
     return (
-        <div className="pt-12 pl-8">
+        <div className="absolute top-4 left-[72%] max-w-[20svw]">
             <div className="flex items-center gap-2">
                 <Avatar avatar={author.avatarUrl} size={'sm'} />
                 <div>
                     <p className="text-sm text-white">{formatFullName(author.firstName, author.lastName)}</p>
-                    <p className="text-sm text-white">{formatRelativeTime(createdAt)}</p>
+                    <p className="text-sm text-white">{formatRelativeTime(photoData.createdAt)}</p>
                 </div>
             </div>
+
+            <div className="py-2">
+                <p className="text-sm text-white">{photoData.caption}</p>
+            </div>
+
+            <PhotoReaction
+                photoId={photoData.photoId}
+                isLikedPhoto={photoData.isLikedPhoto}
+                likesCount={photoData.likesCount}
+                photoReaction={photoReaction}
+            />
         </div>
     )
 }
