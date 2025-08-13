462f2a212a7343bd0df97e16b4e00a830bd64643
Toni <tonirachev92@gmail.com>
2025-08-13 21:34:44 +0300

    refactor: replace user transformation functions with appendFullNameToUser for improved consistency in user data handling

--- lib/utils/transform.ts
+++ lib/utils/transform.ts
@@ -1,7 +1,7 @@
 import { formatFullName } from "@/lib/utils"
 import { CommentDBType, CommentType, PostDBType, PostType, CountType, LikesType } from "@/lib/types/post"
 import { BaseConversationOverviewDBType, ConversationDBType, ConversationOverviewDBType, ConversationOverviewType, ConversationParticipantDBType, MessageDBType } from "../types/conversation"
-import { BaseUserDBType, BaseUserType, UserBiographyDBType, UserProfileDBType, UserProfileType, UserWithPresenceStatusDBType, UserWithPresenceStatusType } from "../types/user"
+import { BaseUserDBType, BaseUserType, UserProfileDBType, UserProfileType, UserWithPresenceStatusDBType, UserWithPresenceStatusType } from "../types/user"
 import { AlbumDBType, AlbumType, PhotoDBType, PhotoType } from "../types/photos"
 
 const extractCount = (item: CountType) => {
@@ -12,42 +12,18 @@ const extractLikes = (likes: LikesType) => {
     return likes.map((like) => like.user)
 }
 
-const extractBiography = (biography: UserBiographyDBType[]) => {
+export const appendFullNameToUser = (user: BaseUserDBType | UserWithPresenceStatusDBType | UserProfileDBType): BaseUserType | UserWithPresenceStatusType | UserProfileType => {
     return {
-        birthDate: biography[0].birth_date,
-        hometown: biography[0].hometown,
-        currentCity: biography[0].current_city,
-        school: biography[0].school,
+        ...user,
+        fullName: formatFullName(user.firstName, user.lastName),
     }
 }
-
-export const transformBaseUser = (user: BaseUserDBType): BaseUserType => ({
-    id: user.id,
-    email: user.email,
-    firstName: user.first_name,
-    lastName: user.last_name,
-    createdAt: user.created_at,
-    fullName: formatFullName(user.first_name, user.last_name),
-    avatarUrl: user.avatar_url,
-})
-
-export const transformUserWithPresenceStatus = (user: UserWithPresenceStatusDBType): UserWithPresenceStatusType => ({
-    ...transformBaseUser(user),
-    presenceStatus: user.presence_status,
-    lastSeen: user.last_seen,
-})
-
-export const transformUserProfile = (user: UserProfileDBType): UserProfileType => ({
-    ...transformUserWithPresenceStatus(user),
-    biography: extractBiography(user.biography),
-    coverUrl: user.cover_url,
-})
 
 export const transformPosts = (posts: PostDBType[], userId?: string): PostType[] => {
     return posts.map((post: PostDBType) => ({
         postId: post.id,
         createdAt: post.created_at,
-        author: transformBaseUser(post.author),
+        author: appendFullNameToUser(post.author),
         body: post.body,
         commentsCount: extractCount(post.commentsCount),
         likesCount: extractCount(post.likesCount),
@@ -62,7 +38,7 @@ export const transformPostComments = (comments: CommentDBType[]): CommentType[] 
         id: comment.id,
         createdAt: comment.created_at,
         authorId: comment.author_id,
-        author: transformBaseUser(comment.author),
+        author: appendFullNameToUser(comment.author),
         postId: comment.post_id,
         body: comment.body,
         likes: extractLikes(comment.likes),
@@ -71,11 +47,11 @@ export const transformPostComments = (comments: CommentDBType[]): CommentType[] 
 }
 
 export const transformFriendRequestsUsers = (users: { user: BaseUserDBType }[]) => {
-    return users.map((user) => transformBaseUser(user.user));
+    return users.map((user) => appendFullNameToUser(user.user));
 }
 
 export const transformFriends = (friends: { friend: UserWithPresenceStatusDBType }[]) => {
-    return friends.map((friend) => transformUserWithPresenceStatus(friend.friend));
+    return friends.map((friend) => appendFullNameToUser(friend.friend));
 }
 
 export const transformMessage = (message: MessageDBType) => {
@@ -126,7 +102,7 @@ export const transformConversationOverview = (overview: ConversationOverviewDBTy
 
     return {
         ...transformBaseConversationOverview(overview),
-        participant: transformUserWithPresenceStatus(participant),
+        participant: appendFullNameToUser(participant) as UserWithPresenceStatusType,
     }
 }
 
@@ -137,7 +113,7 @@ export const transformAlbum = (album: AlbumDBType): AlbumType => {
         type: album.type,
         createdAt: album.created_at,
         photos: album.photos.map((photo) => transformPhoto(photo)),
-        author: transformBaseUser(album.author),
+        author: appendFullNameToUser(album.author),
     }
 }
 
