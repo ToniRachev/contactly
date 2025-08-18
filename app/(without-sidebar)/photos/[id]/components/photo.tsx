ae0ae1079c3a6d007253853b0aad634a8e99666d
Toni <tonirachev92@gmail.com>
2025-08-18 11:22:48 +0300

    refactor: update Photo component to integrate PhotoEngagementStats and PhotoComments, enhancing photo interaction features

--- app/(without-sidebar)/photos/[id]/components/photo.tsx
+++ app/(without-sidebar)/photos/[id]/components/photo.tsx
@@ -5,19 +5,19 @@ import { PhotoType } from "@/lib/types/photos";
 import Image from "next/image";
 import PhotoMetadata from "./photo-metadata";
 import { useAuthenticatedUser } from "@/lib/context/user.context";
-import PhotoReaction from "./photo-reaction";
-import PhotoComment from "./photo-comment";
+import PhotoComments from "./photo-comments";
+import { PhotoCommentsType, PhotoReactionType } from "../lib/types";
+import CommentForm from "@/components/comment/components/comment-form";
+import PhotoEngagementStats from "./photo-engagement-stats";
 
 type PhotoProps = {
     author: BaseUserType;
     photo: PhotoType;
-    photoReaction: {
-        handleOptimisticPhotoReaction: (photoId: string, isLikedPhoto: boolean, userId: string) => void;
-        updateLocalPhotoReaction: (photoId: string, isLikedPhoto: boolean, userId: string) => void;
-    }
+    photoReaction: PhotoReactionType;
+    photoComments: PhotoCommentsType;
 }
 
-export default function Photo({ photo, author, photoReaction }: Readonly<PhotoProps>) {
+export default function Photo({ photo, author, photoReaction, photoComments }: Readonly<PhotoProps>) {
     const { user } = useAuthenticatedUser();
 
     const isLikedPhoto = photo.likes.includes(user.id);
@@ -40,16 +40,25 @@ export default function Photo({ photo, author, photoReaction }: Readonly<PhotoPr
                     }}
                 />
 
-                <PhotoReaction
+                <PhotoEngagementStats
                     photoId={photo.id}
-                    isLikedPhoto={isLikedPhoto}
-                    likesCount={photo.likes.length}
-                    photoReaction={photoReaction}
+                    likes={{
+                        isLiked: isLikedPhoto,
+                        count: photo.likes.length,
+                        reaction: photoReaction
+                    }}
+                    comments={{
+                        count: photo.comments.length
+                    }}
+                />
+                <PhotoComments
+                    comments={photo.comments}
+                    editPhotoComment={photoComments.edit}
+                    deletePhotoComment={photoComments.delete}
                 />
 
-                <PhotoComment
-                    photoId={photo.id}
-                    userId={user.id}
+                <CommentForm
+                    onSubmitSuccess={(content) => photoComments.add(photo.id, content, user)}
                 />
             </div>
         </div>
