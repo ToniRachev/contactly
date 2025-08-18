9322cd5257505ed52d7c4328cdc21fd3491fd901
Toni <tonirachev92@gmail.com>
2025-08-18 11:23:52 +0300

    feat: add PostDetailedView component for displaying post details, comments, and reactions with filtering options

---
+++ components/post/post-detailed-view.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog"
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden"
+import Post from "@/components/post/post"
+import { Separator } from "@/components/ui/separator";
+import Filter from "@/components/filter";
+import { CommentType, PostType } from "@/lib/types/post";
+import CreateComment from "@/components/comment/components/create-comment";
+import Comment from "@/components/comment";
+
+const filters = [
+    {
+        value: 'mostLiked',
+        label: 'Most liked',
+        caption: 'Showing top comments that received the most love.'
+    },
+    {
+        value: 'mostRecent',
+        label: 'Most recent',
+        caption: 'What people are saying right now.'
+    }
+]
+
+type Controls = {
+    setState: (state: boolean) => void;
+    open: boolean;
+}
+
+type PostViewProps = {
+    post: PostType;
+    controls: Controls;
+    reaction: () => void;
+    commentState: {
+        isLikedPost: boolean;
+        comments: CommentType[];
+        addComment: (comment: CommentType) => void;
+        editComment: (commentId: string, newContent: string) => void;
+        deleteComment: (commentId: string) => void;
+        reactionComment: (commentId: string, userId: string, isLikedComment: boolean) => void;
+        commentsLoading: boolean;
+    }
+}
+
+export function PostDetailedView(
+    {
+        post,
+        controls,
+        reaction,
+        commentState
+    }: Readonly<PostViewProps>) {
+    return (
+        <Dialog open={controls.open} onOpenChange={controls.setState}>
+            <DialogContent className="min-w-[35vw] border-none">
+                <DialogHeader>
+                    <VisuallyHidden>
+                        <DialogTitle>Post view</DialogTitle>
+                    </VisuallyHidden>
+                </DialogHeader>
+                <div className="flex flex-col gap-4 relative max-h-[80vh] overflow-y-auto pr-4
+                      [&::-webkit-scrollbar]:w-2 
+                    [&::-webkit-scrollbar-track]:bg-surface
+                    [&::-webkit-scrollbar-thumb]:bg-[#8C8C8C]
+                    [&::-webkit-scrollbar-button]:hidden
+                ">
+                    <Post post={post} reaction={reaction} isLikedPost={commentState.isLikedPost} />
+
+                    <Separator className="opacity-25" />
+                    <Filter filters={filters} />
+
+
+                    {commentState.comments.map((comment) => (
+                        <Comment
+                            key={comment.id}
+                            author={comment.author}
+                            comment={comment}
+                            editComment={commentState.editComment}
+                            deleteComment={commentState.deleteComment}
+                            commentReaction={commentState.reactionComment}
+                        />
+                    ))}
+
+                    <CreateComment postId={post.postId} addComment={commentState.addComment} />
+                </div>
+            </DialogContent>
+        </Dialog>
+    )
+}
