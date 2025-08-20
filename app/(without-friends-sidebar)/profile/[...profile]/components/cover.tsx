169bcc23ef7015c0ef43988de6962d716629480d
Toni <tonirachev92@gmail.com>
2025-08-20 21:19:47 +0300

    refactor: adjust cover component layout for consistent height and improved styling

--- app/(without-friends-sidebar)/profile/[...profile]/components/cover.tsx
+++ app/(without-friends-sidebar)/profile/[...profile]/components/cover.tsx
@@ -7,15 +7,17 @@ type CoverProps = {
 export default function Cover({ coverUrl }: Readonly<CoverProps>) {
 
     return (
-        coverUrl ? (
-            <Image
-                src={coverUrl}
-                alt="User cover photo"
-                fill
-                className="object-cover object-center"
-            />
-        ) : (
-            <div className="w-full h-full absolute inset-0 bg-surface" />
-        )
+        <div className="h-[50svh] relative">
+            {coverUrl ? (
+                <Image
+                    src={coverUrl}
+                    alt="User cover photo"
+                    fill
+                    className="object-cover object-center"
+                />
+            ) : (
+                <div className="w-full h-[50svh] absolute inset-0 bg-surface" />
+            )}
+        </div>
     )
 }
