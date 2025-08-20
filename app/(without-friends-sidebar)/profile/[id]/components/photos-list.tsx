8ad2e29b3f1edc5d55eea1c01c1fd57ba524faa1
Toni <tonirachev92@gmail.com>
2025-08-20 16:20:30 +0300

    refactor: enhance PhotosList component to include empty state and link to all photos

--- app/(without-friends-sidebar)/profile/[id]/components/photos-list.tsx
+++ app/(without-friends-sidebar)/profile/[id]/components/photos-list.tsx
@@ -1,21 +1,50 @@
 import { PhotoType } from "@/lib/types/photos";
 import Image from "next/image";
 import SectionWrapper from "./section-wrapper";
+import Link from "next/link";
 
 type PhotosListProps = {
     photos: PhotoType[];
+    profileId: string;
 }
 
-export default function PhotosList({ photos }: Readonly<PhotosListProps>) {
+const List = ({ photos }: Readonly<{ photos: PhotoType[] }>) => {
+    return (
+        <ul className="grid grid-cols-3 gap-3">
+            {photos.map((photo) => (
+                <li key={photo.id}>
+                    <Link href={`/photos/${photo.id}`}>
+                        <Image src={photo.url} alt={photo.caption ?? ''} width={100} height={100} className="w-full h-full object-cover" />
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
+
+const EmptyList = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-full">
+            <p>No photos yet</p>
+        </div>
+    )
+}
+
+export default function PhotosList({ photos, profileId }: Readonly<PhotosListProps>) {
+    const hasPhotos = photos.length > 0;
+    const ListComponent = hasPhotos ? List : EmptyList;
+
     return (
         <SectionWrapper title="Photos">
-            <ul className="grid grid-cols-3 gap-3">
-                {photos.map((photo) => (
-                    <li key={photo.id}>
-                        <Image src={photo.url} alt={photo.caption ?? ''} width={100} height={100} className="w-full h-full object-cover" />
-                    </li>
-                ))}
-            </ul>
+            {hasPhotos && (
+                <div className="absolute top-5 right-4">
+                    <Link href={`/profile/${profileId}/photos`}>
+                        See all photos
+                    </Link>
+                </div>
+            )}
+
+            <ListComponent photos={photos} />
         </SectionWrapper>
     )
 }
