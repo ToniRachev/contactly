ab33394ba6768e1924a30f3ee945eb2825b95776
Toni <tonirachev92@gmail.com>
2025-08-20 21:47:44 +0300

    refactor: enhance UserData component to include friend request functionality based on friendship status

--- app/(without-friends-sidebar)/profile/[...profile]/components/user-data.tsx
+++ app/(without-friends-sidebar)/profile/[...profile]/components/user-data.tsx
@@ -1,13 +1,22 @@
+'use client';
+
 import UserProfileCard from "@/components/user-profile-card";
 import EditProfile from "./edit-profile";
+import { useFriends } from "@/lib/context/friends.context";
+import FriendRequestButton from "@/components/friend-request-button";
 
 type UserDataProps = {
     fullName: string;
     avatarUrl: string | null;
     isOwnProfile: boolean;
+    profileId: string;
 }
 
-export default function UserData({ fullName, avatarUrl, isOwnProfile }: Readonly<UserDataProps>) {
+export default function UserData({ fullName, avatarUrl, isOwnProfile, profileId }: Readonly<UserDataProps>) {
+    const { friends } = useFriends();
+
+    const areFriends = friends.some(friend => friend.id === profileId);
+
     return (
         <div className="flex justify-between w-full">
             <div className="flex items-center justify-between w-full">
@@ -21,6 +30,10 @@ export default function UserData({ fullName, avatarUrl, isOwnProfile }: Readonly
                     <div className="">
                         <EditProfile />
                     </div>
+                )}
+
+                {!areFriends && !isOwnProfile && (
+                    <FriendRequestButton receiverId={profileId} />
                 )}
             </div>
         </div>
