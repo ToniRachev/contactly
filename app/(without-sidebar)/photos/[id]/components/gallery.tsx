7e4dbc8e5210cb580838c2ea969bb7513ddbe2af
Toni <tonirachev92@gmail.com>
2025-08-20 21:59:36 +0300

    refactor: remove author prop from Gallery component and directly access active photo's author for cleaner code

--- app/(without-sidebar)/photos/[id]/components/gallery.tsx
+++ app/(without-sidebar)/photos/[id]/components/gallery.tsx
@@ -5,18 +5,14 @@ import { useEffect } from "react";
 import CloseButton from "./close-button";
 import Navigation from "./navigation";
 import Photo from "./photo";
-import { BaseUserType } from "@/lib/types/user";
 import usePhotos from "../lib/hooks/usePhotos";
 
 type GalleryProps = {
     initialPhotos: PhotoType[];
     activePhotoId: string;
-    author: BaseUserType;
 }
 
-// TODO: Add photo alt
-
-export default function Gallery({ initialPhotos, activePhotoId, author }: Readonly<GalleryProps>) {
+export default function Gallery({ initialPhotos, activePhotoId }: Readonly<GalleryProps>) {
     const { photos, galleryNavigation, togglePhotoReaction, photoComments } = usePhotos(initialPhotos, activePhotoId);
 
     useEffect(() => {
@@ -25,13 +21,15 @@ export default function Gallery({ initialPhotos, activePhotoId, author }: Readon
 
     const albumHasMoreThanOnePhoto = photos.length > 1;
 
+    const activePhoto = photos[galleryNavigation.activePhotoIndex];
+
     return (
         <div className="w-full flex justify-center items-center relative">
             <CloseButton />
 
             <Photo
-                author={author}
-                photo={photos[galleryNavigation.activePhotoIndex]}
+                author={activePhoto.author}
+                photo={activePhoto}
                 photoReaction={togglePhotoReaction}
                 photoComments={photoComments}
             />
