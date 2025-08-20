aec499093cef4e7647baf888cc4637874425864d
Toni <tonirachev92@gmail.com>
2025-08-20 21:20:00 +0300

    refactor: update UserData component to include EditProfile functionality for user profiles

--- app/(without-friends-sidebar)/profile/[...profile]/components/user-data.tsx
+++ app/(without-friends-sidebar)/profile/[...profile]/components/user-data.tsx
@@ -1,18 +1,28 @@
 import UserProfileCard from "@/components/user-profile-card";
+import EditProfile from "./edit-profile";
 
 type UserDataProps = {
     fullName: string;
     avatarUrl: string | null;
+    isOwnProfile: boolean;
 }
 
-export default function UserData({ fullName, avatarUrl }: Readonly<UserDataProps>) {
+export default function UserData({ fullName, avatarUrl, isOwnProfile }: Readonly<UserDataProps>) {
     return (
-        <div>
-            <UserProfileCard
-                name={fullName}
-                className="items-start"
-                avatar={avatarUrl}
-            />
+        <div className="flex justify-between w-full">
+            <div className="flex items-center justify-between w-full">
+                <UserProfileCard
+                    name={fullName}
+                    className="items-center flex-row"
+                    avatar={avatarUrl}
+                />
+
+                {isOwnProfile && (
+                    <div className="">
+                        <EditProfile />
+                    </div>
+                )}
+            </div>
         </div>
     )
 }
